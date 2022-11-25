7ce2b6c60013f6dbd8cd26ee8308a97bbd23ee4b
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-11-25 08:47:15 +0600

    register

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -3,7 +3,81 @@ import { Link } from "react-router-dom";
 import img from "../../assets/images/login-pic.webp";
 
 const Register = () => {
-  return <div></div>;
+  return (
+    <div className="hero w-full my-20">
+      <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+          <img src={img} alt="" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <h1 className="text-5xl text-center font-bold">Register </h1>
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                name="photoURL"
+                placeholder="Your Photo URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <input
+                className="btn btn-primary"
+                type="submit"
+                value="Register"
+              />
+            </div>
+          </form>
+          <p className="text-center">
+            Already have an account
+            <Link to="/login" className="text-primary font-bold mx-2 ">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
