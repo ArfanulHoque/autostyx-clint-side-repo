2fbde91d0f1fccc9bdeca998d887284ba06a8995
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-11-29 08:49:04 +0600

    all buyers

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,9 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const AllBuyers = () => {
+  const url = "http://localhost:5000/users?role=buyer";
+
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data;
+    },
+  });
+  console.log(users);
+
   return (
     <div>
-      <h2>All Buyers</h2>
+      <h2 className="text-4xl text-center mb-3">All Buyers</h2>
+
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, i) => (
+              <tr>
+                <th>{i + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button className="btn  btn-xs btn-primary">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,9 +1,51 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const AllSellers = () => {
+  const url = "http://localhost:5000/users?role=saler";
+
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data;
+    },
+  });
+  console.log(users);
+
   return (
     <div>
-      <h2>All Sellers</h2>
+      <h2 className="text-4xl text-center mb-3">All Sellers</h2>
+
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Verify</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, i) => (
+              <tr key={user._id}>
+                <th>{i + 1}</th>
+                <td>{user.email}</td>
+                <td>{user.name}</td>
+                <td>
+                  <button className="btn  btn-xs btn-primary">Verify</button>
+                </td>
+                <td>
+                  <button className="btn  btn-xs btn-primary">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
